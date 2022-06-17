ec5930e7b8c27fda8294232910cb0a71f942f51c
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-17 23:26:04 +0300

    Created the profile model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,22 @@
+from datetime import datetime
+from datetime import datetime
 from django.db import models
+from django.contrib.auth import get_user_model
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+User = get_user_model()
+
+# User profile model
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_image=CloudinaryField('image', blank=True)
+    date_joined=models.DateTimeField(default=datetime.now)
+    
+    
+    
+
+    def __str__(self):
+        return self.user.username
