73af4495603ca6bffb614cacc947460cd50ca4ed
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-06-19 20:55:36 +0300

    Added the search bussiness url to display search results

--- hood/urls.py
+++ hood/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('feed/', views.feed, name='feed'),
     path('businesses/', views.businesses, name='businesses'),
     path('neigborhood/', views.neighborhood, name='neighborhood'),
+    path('search_businesses/', views.search, name='search'),
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('logout/', views.logout, name='logout'),
+
+    
 ]
